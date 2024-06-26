1ea99415a71b9382ad0c2f554e7d736fffc4625d
Mavine1 <mavinemalin@gmail.com>
2024-06-26 13:10:00 +0300

    region improve separate him slope

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,9 @@ import { Link} from "react-router-dom";
 
 export default function Header() {
   return (
-  <Navbar className="border-b-2">
-    <Link to="/" className="self-center whitespace-nowrap text-sm:text-xl">
+  <Navbar className="border-b-">
+    <Link to="/" className="self-center whitespace-nowrap text-sm:text-xl font-semibold
+    dark:text-white">
     <span>David</span>
     Blog
     
