6d40f848c269f01ef3c85bb6e0fd46c2bec4b1ff
Mavine1 <mavinemalin@gmail.com>
2024-06-26 12:10:00 +0300

    pour hunt movie crowd snow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Home } from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path = "/about" element = {<About/>} />
-        <Route path = "/sign-in" element = {<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/project" element={<Project />} />
@@ -20,3 +20,5 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
+export default App;
