d5a7e6572cec5f2f54994244217e1cee70059a31
Mavine1 <mavinemalin@gmail.com>
2024-06-26 13:20:01 +0300

    measure reach opportunity think troops

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
   <Navbar className="border-b-2">
     <Link to="/" className="self-center whitespace-nowrap text-sm:text-xl font-semibold
     dark:text-white">
-    <span className="px-2 py-1 bg-gradient-to-r from-indigo-500">David</span>
+    <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
+    via-purple-500 to-pink-500">David</span>
     Inspirations
     </Link>
 
