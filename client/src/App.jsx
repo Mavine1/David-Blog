faf2286bca08b5696d427fcf0b089deee6c7114e
Mavine1 <mavinemalin@gmail.com>
2024-06-26 11:56:00 +0300

    stomach cast roar wing load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/SignIn";
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
         <Route path = "/about" element = {<About/>} />
         <Route path = "/sign-in" element = {<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard</>} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
