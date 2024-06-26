bfd666cacdc1e5cfa42777b8019a6631bdce19dc
Mavine1 <mavinemalin@gmail.com>
2024-06-26 11:46:00 +0300

    shirt score train seldom coast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path="" element={<Home/>/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
