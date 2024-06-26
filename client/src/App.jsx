42d518522ac005193c97768a943936aeb12092cc
Mavine1 <mavinemalin@gmail.com>
2024-06-26 11:44:00 +0300

    vessels waste biggest far place

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-        <Route></Route>
+        <Route path="" element={<Home/>/>
     </Routes>
     </BrowserRouter>
   )
