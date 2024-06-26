1e50741b35aa4ca6acab4a513af1fb1e5f3cc618
Mavine1 <mavinemalin@gmail.com>
2024-06-26 11:52:00 +0300

    friendly fight mind movie operation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
   return (
@@ -8,7 +9,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path = "/about" element = {<About/>} />
-        <Route path = "*" element = {<Home/>} />
+        <Route path = "/sign-in" element = {<SignIn />} />
+        <Route path="/"
       </Routes>
     </BrowserRouter>
   );
