ce62f0f2477f55b85ce74e17a479e3f491bf7b29
Mavine1 <mavinemalin@gmail.com>
2024-06-26 13:06:01 +0300

    cave darkness tales pain happen

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Navbar } from "flowbite-react";
-import { Link}
+import { Link} from "react-router-dom";
 
 export default function Header() {
   return (
   <Navbar className="border-b-2">
+    <Link to="/" className="self-center "
 
   </Navbar>
   )
