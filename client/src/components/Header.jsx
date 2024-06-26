3395dc7f1699c2966c3462916e614ce34f346f47
Mavine1 <mavinemalin@gmail.com>
2024-06-26 13:36:00 +0300

    fell tall shout piano liquid

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     dark:text-white">
     <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
     via-purple-500 to-pink-500 rounded-lg text-white">David</span>
-    Inspirations
+     
     </Link>
 
   </Navbar>
