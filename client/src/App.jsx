d6f87849d7e1b0ecec4b4ce5dc93f5c753cc4dfe
Mavine1 <mavinemalin@gmail.com>
2024-06-26 12:14:01 +0300

    whose too bus rear automobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,21 @@ import Project from "./pages/Project";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
+      <div className="min-h-screen bg-gray-100">
+        <nav className="bg-white shadow-md p-4">
+          {/* Add navigation items here */}
+        </nav>
+        <main className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/project" element={<Project />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
