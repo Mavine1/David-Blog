4438215a5a55dee67be00bdbd683ad60e1d9aac2
Mavine1 <mavinemalin@gmail.com>
2024-06-26 11:58:01 +0300

    difference orbit previous outside atom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
