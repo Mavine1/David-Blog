4aac6e3a8843740cc791bd81ca026178e078d34b
Mavine1 <mavinemalin@gmail.com>
2024-06-27 16:52:00 +0300

    office transportation your dropped material

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <Navbar className="border-b-2">
-      <Link to="/" className="self px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+      <Link to="/" className="self-center whitespace-nowrap px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
       <span>David</span>
       <em>Inspirations</em>
-      
       </Link>
     </Navbar>
   )
 }
+ 
