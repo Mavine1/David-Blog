c157b8262e1beed9bf51a4ece97bd83be60ed663
Mavine1 <mavinemalin@gmail.com>
2024-06-27 16:40:00 +0300

    sight stronger pull run evidence

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,16 @@
 import flowbite from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    
+    flowbite,
   ],
-}
+};
