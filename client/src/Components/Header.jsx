ee1e59096e3f11d4457ecb1c688b51a1946a3566
Mavine1 <mavinemalin@gmail.com>
2024-06-27 18:26:00 +0300

    truck talk home whenever deep

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
         className="hidden lg:inline"
          />
       </form>
-      <Button className="lg:hidden hover:bg-gray-300" color="gray" pill>
+      <Button className="lg:hidden hover:bg-gray-300 " color="gray" pill>
       <AiOutlineSearch/>
       </Button>
       <div className="flex gap-2 md:order-2">
