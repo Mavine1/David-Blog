23f05c862cfd882e7afd3ac3de3c20b785cdc617
Mavine1 <mavinemalin@gmail.com>
2024-06-27 17:46:00 +0300

    success too go said deal

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -14,9 +14,7 @@ export default function Header() {
         <TextInput 
         type="text"
         placeholder="Search..."
-        className="pl-10 pr-4 py-2"
          />
-         <AiOutlineSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
       </form>
       <Button color="gray" pill>
       <AiOutlineSearch/>
