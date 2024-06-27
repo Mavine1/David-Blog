3d421a2908b14e6c8b8fa7ac611daf3b0a3195af
Mavine1 <mavinemalin@gmail.com>
2024-06-27 13:46:01 +0300

    origin sleep carried announced pick

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,29 +1,6 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link} from "react-router-dom";
-import {AiOutlineSearch} from "react-icons/ai";
 
 export default function Header() {
   return (
-    <Navbar className=" border-2">
-      <Link
-        to="/"
-        className="-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          David
-        </span>
-        <em>Inspirations</em>
-      </Link>
-      <form className="flex items-center justify-center flex-1 lg:mr-32">
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          className="hidden lg:inline"
-        />
-      </form>
-      <Button className="hidden lg:inline">
-        <AiOutlineSearch />
-      </Button>
-    </Navbar>
-  );
+    <div>Header</div>
+  )
 }
