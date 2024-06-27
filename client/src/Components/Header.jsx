8bc6ce650c9738884d011b2a6854681c8fb291a6
Mavine1 <mavinemalin@gmail.com>
2024-06-27 18:50:01 +0300

    fill clay mile pencil regular

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,42 +1,42 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
-import {AiOutlineSearch} from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-      <span className="px-4  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-        David</span>
-      <em>Inspirations</em>
+        <span className="px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          David
+        </span>
+        <em>Inspirations</em>
       </Link>
-      <form className="py-1 flex items-center justify-center flex-1 lg:mr-32" >
-        <TextInput 
-        type="text"
-        placeholder="Search..."
-        className="hidden lg:inline"
-         />
+      <form className="py-1 flex items-center justify-center flex-1 lg:mr-32">
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          className="hidden lg:inline"
+        />
       </form>
       <Button className="lg:hidden hover:bg-gray-300" color="gray" pill>
-      <AiOutlineSearch/>
+        <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="hidden sm:inline hover:bg-gray-300 "  color="gray" pill>
-        <FaMoon />  
+        <Button className="hidden sm:inline hover:bg-gray-300" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button className="sm:inline bg-gradient-to-r from-purple-500 to-blue-500 hover:bg-gray-300" color="gray" pill>
+            Sign In
           </Button>
-          <Link to="/sign-in">
-          <Button className="sm:inline bg-gradient-to-r from-purple-500 to-blue-500 hover:bg-gray-300"  color="gray" pill>Sign In</Button>
-          </Link> 
-          <Navbar.Collapse>
-            <Navbar.Link>
-             <Link to="/">
-             Home
-             </Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
+        </Link>
+        <Navbar.Collapse>
+          <Navbar.Link>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </div>
     </Navbar>
   );
 }
- 
